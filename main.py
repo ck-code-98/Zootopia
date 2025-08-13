99a5df6a9f02b2de5c28c2906391934abfc693f5
ck-code-98 <constantin-knappe@web.de>
2025-08-13 16:09:50 +0200

    Implemented HTML output based on user input

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ API_KEY = "f34fZuedb6TQDadSf68Wsw==Qg7HEFhopF0PVHNz"
 def load_data_from_api(animal_name):
     url = f"{BASE_URL}name={animal_name}"
     response = requests.get(url, headers={"X-Api-Key": API_KEY})
-    print(response)
     data = response.json()
+    if len(data) == 0:
+        no_output_message = f"<h2>The animal {animal_name} doesn't exist.</h2>"
+        return no_output_message
     return data
 
 
@@ -52,20 +54,23 @@ def serialize_animal(animal):
 def generate_html_code(overall_data):
     """ iterates through all animals """
     output = ""
-    for animal in overall_data:
-        output += serialize_animal(animal)
+    if not type(overall_data) == list:
+        output = f'<li class="cards__item">	<div class="card__title">{overall_data}</div></li>'
+    else:
+        for animal in overall_data:
+            output += serialize_animal(animal)
     return output
 
 
 def main():
-    animal_name = input("Enter your animal: ")
-    print(load_data_from_api(animal_name))
-    #overall_animals_data = load_data_from_json('animals_data.json')
-    #desired_output = generate_html_code(overall_animals_data)
-    #original_html_content = load_data_from_html('animals_template.html')
-    #updated_html_content = original_html_content.replace("__REPLACE_ANIMALS_INFO__", desired_output)
-    #updated_html_content = updated_html_content.replace('<style>', '<meta charset="UTF-8" /> \n\t<style>')
-    #write_new_html_file('animals.html', updated_html_content)
+    animal_name = input("Enter a name of an animal: ")
+    animals_data = load_data_from_api(animal_name.lower())
+    desired_output = generate_html_code(animals_data)
+    original_html_content = load_data_from_html('animals_template.html')
+    updated_html_content = original_html_content.replace("__REPLACE_ANIMALS_INFO__", desired_output)
+    updated_html_content = updated_html_content.replace('<style>', '<meta charset="UTF-8" /> \n\t<style>')
+    write_new_html_file('animals.html', updated_html_content)
+    print("Website was successfully generated to the file animals.html")
 
 
 if __name__ == "__main__":
